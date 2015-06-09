d7aa1aef631735ebb0189a8baccb22846aff0c10
Evandro Zanatta <ezanatta@tray.net.br>
2015-06-09 09:51:57 -0300

    Criando instalador local

---
+++ download_and_install_opencode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget http://tray-tecnologia.github.io/theme-template/assets/gems/opencode_theme-0.0.2.gem
+gem install opencode_theme-0.0.2.gem
+
+wget https://raw.githubusercontent.com/tray-tecnologia/opencode-workflow/master/gulpfile.js
+wget https://raw.githubusercontent.com/tray-tecnologia/opencode-workflow/master/package.json
+
+npm install
---
+++ install_opencode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gem install opencode_theme-0.0.2.gem
+npm install
