b4293a22545f9507fd2c93a6517af7aa7d77c7b0
Evandro Zanatta <ezanatta@tray.net.br>
2015-06-09 09:55:18 -0300

    Configurando instalador remoto

--- download_and_install_opencode.sh
+++ download_and_install_opencode.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget http://tray-tecnologia.github.io/theme-template/assets/gems/opencode_theme-0.0.2.gem
+wget https://github.com/Tray-FrontEnd/FastInstallOpenCode/raw/master/opencode_theme-0.0.2.gem
 gem install opencode_theme-0.0.2.gem
 
-wget https://raw.githubusercontent.com/tray-tecnologia/opencode-workflow/master/gulpfile.js
-wget https://raw.githubusercontent.com/tray-tecnologia/opencode-workflow/master/package.json
+wget https://raw.githubusercontent.com/Tray-FrontEnd/FastInstallOpenCode/master/gulpfile.js
+wget https://raw.githubusercontent.com/Tray-FrontEnd/FastInstallOpenCode/master/package.json
 
 npm install
