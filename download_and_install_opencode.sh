989bbe141ea2796d5b3058684837fc10c4272383
Evandro Zanatta <ezanatta@tray.net.br>
2015-06-09 10:17:20 -0300

    Update download_and_install_opencode.sh

--- download_and_install_opencode.sh
+++ download_and_install_opencode.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Baixando os arquivos"
 wget https://github.com/Tray-FrontEnd/FastInstallOpenCode/raw/master/opencode_theme-0.0.2.gem
-gem install opencode_theme-0.0.2.gem
-
 wget https://raw.githubusercontent.com/Tray-FrontEnd/FastInstallOpenCode/master/gulpfile.js
 wget https://raw.githubusercontent.com/Tray-FrontEnd/FastInstallOpenCode/master/package.json
 
+echo "Instalando OpenCode"
+gem install opencode_theme-0.0.2.gem
+
+echo "Instalando as dependências com NPM"
 npm install
